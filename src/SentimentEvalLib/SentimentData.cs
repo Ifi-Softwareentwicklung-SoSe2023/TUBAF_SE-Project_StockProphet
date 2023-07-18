c735cf3fd7ae49ed96c18efbfc3704a48d4d0265
Martin Werner <mw35fowi@tu-freiberg.de>
2023-07-18 12:17:48 +0200

    Declare members and methods within SentimentEvalLib (#6)

--- src/SentimentEvalLib/SentimentData.cs
+++ src/SentimentEvalLib/SentimentData.cs
@@ -1,14 +1,23 @@
-using System;
+using Microsoft.ML.Data;
 
 namespace SentimentEvalLib
 {
-    class SentimentData
+    public class SentimentData
     {
+        [LoadColumn(0)]
+        public string? SentimentText;
 
+        [LoadColumn(1), ColumnName("Label")]
+        public bool Sentiment;
     }
 
-    class SentimentPrediction : SentimentData
+    public class SentimentPrediction : SentimentData
     {
-        
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Probability { get; set; }
+
+        public float Score { get; set; }
     }
 }
--- src/SentimentEvalLib/SentimentEvaluator.cs
+++ src/SentimentEvalLib/SentimentEvaluator.cs
@@ -1,19 +1,43 @@
 using System;
 
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using SentimentAnalysis;
+using static Microsoft.ML.DataOperationsCatalog;
+
+
 namespace SentimentEvalLib
 {
-    interface ISentiment
-    {
-
-    }
-
     interface ISentimentTrain
     {
-
+        void LoadData(string path);
+        void CreateModel();
+        void Fit();
+        void Test();
+        void SaveModel(string path);
     }
 
-    class SentimentEvaluator : ISentiment, ISentimentTrain
+
+    interface ISentiment
     {
-        
+        void LoadModel(string path);
+        SentimentPrediction Evaluate(SentimentData data);
+    }
+
+
+    class SentimentEvaluator : ISentimentTrain, ISentiment
+    {
+        private MLContext MlContext;
+        private TrainTestData SplitDataView;
+        private ITransformer Model;
+
+        public void ISentimentTrain.LoadData(string path) {}
+        public void ISentimentTrain.CreateModel() {}
+        public void ISentimentTrain.Fit() {}
+        public void ISentimentTrain.Test() {}
+        public void ISentimentTrain.SaveModel(string path) {}     
+
+        public void ISentiment.LoadModel(string path) {}
+        public SentimentPrediction ISentiment.Evaluate(SentimentData data) {}
     }
 }
