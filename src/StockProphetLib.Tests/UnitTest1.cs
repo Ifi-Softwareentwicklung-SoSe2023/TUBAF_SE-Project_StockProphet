68bd9d600476e2b94ab28bc172160e9fd42d9fc2
Martin Werner <m.werner.0@protonmail.com>
2023-07-19 21:16:47 +0200

    Init xUnit test projects for each library

--- src/StockProphetLib.Tests/UnitTest1.cs
+++ src/StockProphetLib.Tests/UnitTest1.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
 
-namespace UnitTests
+namespace StockProphetLib.Tests
 {
     public class UnitTest1
     {
---
+++ src/WebCollectorLib.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WebCollectorLib.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
