452d1f039ef9466489c21412f20f6813b83075cd
Martin Werner <m.werner.0@protonmail.com>
2023-07-21 00:27:32 +0200

    Create tests for ISentiment.Evaluate method

--- src/SentimentEvalLib.Tests/SentimentEvaluatorTests.cs
+++ src/SentimentEvalLib.Tests/SentimentEvaluatorTests.cs
@@ -6,10 +6,31 @@ namespace SentimentEvalLib.Tests
 {
     public class SentimentEvaluatorTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("Things are going well.")]
+        [InlineData("It's not looking good.")]
+        public void Evaluate_ShouldReturnValidPredictionProbability(string text)
         {
-            
+            // Arrange
+            ISentiment evaluator = new SentimentEvaluator();
+            SentimentData data = new SentimentData { SentimentText = text };
+
+            // Act
+            float actual = evaluator.Evaluate(data).Probability;
+
+            // Assert
+            Assert.InRange<float>(actual, 0.0f, 1.0f);
+        }
+
+        [Fact]
+        public void Evaluate_ShouldThrowOnEmpty()
+        {
+            // Arrange
+            ISentiment evaluator = new SentimentEvaluator();
+            SentimentData data = new SentimentData { SentimentText = "" };
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => evaluator.Evaluate(data));
         }
     }
 }
--- src/SentimentEvalLib/SentimentEvaluator.cs
+++ src/SentimentEvalLib/SentimentEvaluator.cs
@@ -2,13 +2,12 @@ using System;
 
 using Microsoft.ML;
 using Microsoft.ML.Data;
-using SentimentAnalysis;
 using static Microsoft.ML.DataOperationsCatalog;
 
 
 namespace SentimentEvalLib
 {
-    interface ISentimentTrain
+    public interface ISentimentTrain
     {
         void LoadData(string path);
         void CreateModel();
@@ -18,26 +17,28 @@ namespace SentimentEvalLib
     }
 
 
-    interface ISentiment
+    public interface ISentiment
     {
         void LoadModel(string path);
         SentimentPrediction Evaluate(SentimentData data);
     }
 
 
-    class SentimentEvaluator : ISentimentTrain, ISentiment
+    public class SentimentEvaluator : ISentimentTrain, ISentiment
     {
         private MLContext MlContext;
         private TrainTestData SplitDataView;
         private ITransformer Model;
 
-        public void ISentimentTrain.LoadData(string path) {}
-        public void ISentimentTrain.CreateModel() {}
-        public void ISentimentTrain.Fit() {}
-        public void ISentimentTrain.Test() {}
-        public void ISentimentTrain.SaveModel(string path) {}     
-
-        public void ISentiment.LoadModel(string path) {}
-        public SentimentPrediction ISentiment.Evaluate(SentimentData data) {}
+        void ISentimentTrain.LoadData(string path) {}
+        void ISentimentTrain.CreateModel() {}
+        void ISentimentTrain.Fit() {}
+        void ISentimentTrain.Test() {}
+        void ISentimentTrain.SaveModel(string path) {}       
+        
+        void ISentiment.LoadModel(string path) {}
+        SentimentPrediction ISentiment.Evaluate(SentimentData data) {
+            return new SentimentPrediction { Probability = -1.0f };
+        }
     }
 }
