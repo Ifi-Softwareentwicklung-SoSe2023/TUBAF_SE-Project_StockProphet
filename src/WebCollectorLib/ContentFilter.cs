91c31bb759ff02b54e34483e8cf03325c5f3e6aa
Ben Weckend <bw19jivu@tu-freiberg.de>
2023-07-22 15:09:30 +0200

    Filter added

--- src/WebCollectorLib/ContentFilter.cs
+++ src/WebCollectorLib/ContentFilter.cs
@@ -1,12 +1,32 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace WebCollectorLib
 {
-   /* static class ContentFilter
+    static class ContentFilter
     {
-        public static string RemoveAuthors(string Text) {}
-        public static string RemoveCharacters(string Text) {}
-        public static string ReplaceCharacters(string Text) {}
-    } 
-    */  
+        public static string RemoveAuthors(string Text)
+        {
+            // Entfernt alle Paragraphen, die Autor/ Autorenen enthalten
+            string[] paragraphs = Text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            string filteredText = "";
+            foreach (string paragraph in paragraphs)
+            {
+                if (!paragraph.Contains("Autor") && !paragraph.Contains("Autoren"))
+                {
+                    filteredText += paragraph + Environment.NewLine;
+                }
+            }
+
+            return filteredText;
+        }
+
+        public static string RemoveCharacters(string Text)
+        {
+            // Entferne alle Emojis
+            string noEmojiText = Regex.Replace(Text, @"[\p{Cs}\p{Co}\p{Cf}\p{Lm}\p{Sk}\p{P}\p{S}]", "");
+
+            return noEmojiText;
+        }
+    }
 }
