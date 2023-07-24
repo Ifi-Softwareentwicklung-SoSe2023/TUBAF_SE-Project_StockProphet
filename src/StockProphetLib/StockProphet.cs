3fc964d05c88ab49eaa86a41338139b3af643d32
Martin Werner <mw35fowi@tu-freiberg.de>
2023-07-24 15:41:50 +0200

    Update Article class and implement methods of StockProphet class (#13)

--- src/StockProphetLib/StockProphet.cs
+++ src/StockProphetLib/StockProphet.cs
@@ -13,10 +13,73 @@ namespace StockProphetLib
         private List<Article> Articles;
         private List<IArticleHandler> Handlers;
 
-        public StockProphet() {}
+        public StockProphet() 
+        {
+            Handlers.Add(new Collector());
+            Handlers.Add(new PreProcessor());
+            Handlers.Add(new Evaluator());
+        }
         
-        public float Prophesy(string keyWord) { return new float(); }
+        public float Prophesy(string KeyWord) 
+        { 
+            LinkCollector linkCollector = new LinkCollector();
+            string[] links = await linkCollector.FindLinks(KeyWord);  
+         
+            CreateArticles(links);
+            RunTasks();         
+            return SumSentiment();
+        }
 
-        private void StartTasks() {}
+        private void CreateArticles(string[] Links) 
+        {
+            foreach (var ln in Links)
+            {
+                Article article = new Article();
+                article.Link = ln;
+                Articles.Add(article);
+            }
+        }
+
+        private void RunTasks() 
+        {
+            // prepare multicast delegate
+            Action<object> chain;
+            foreach (var handler in Handlers) {
+                chain += handler.Run;
+            }
+
+            Task[] tasks = new Task[Articles.Count];
+
+            // start tasks
+            int i = 0;
+            foreach (var article in Articles)
+            {
+                tasks[i] = new Task(chain, (object)article);
+                tasks[i++].Start();
+            }
+
+            // wait for tasks to finish
+            try 
+            {
+                Task.WaitAll(tasks);
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("Aggregation failed: {0}", e.Message);
+            }
+        }
+
+        private float SumSentiment() 
+        {
+            float sum = 0;
+            int count = 0;
+            foreach (var article in Articles)
+            {
+                sum += article.Sentiment;
+                count++;
+            }
+
+            return sum / count;
+        }
     }
 }
